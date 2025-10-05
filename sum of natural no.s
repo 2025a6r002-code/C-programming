32c3dd02c1fe74a78e9d7e7be26b478f56890082
2025a6r002-code <2025a6r002@mietjammu.in>
2025-10-05 21:15:09 +0530

    Create sum of natural no.s

---
+++ sum of natural no.s
@@ -0,0 +1,17 @@
+//WAP to calculate the sum of n natural numbers.
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    
+    int sum=0;
+    for(int i=1;i<=N;i++){
+        sum=sum+i;
+    }
+      printf("%d",sum);
+    return 0;
+}
